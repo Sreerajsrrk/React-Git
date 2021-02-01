46a82342d88f8bdff13e5044b642a0a0fe9825e2
Sreeraj <sreeraj@Sreerajs-Air.Dlink>
2021-02-01 15:58:06 +0530

    final try

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const element = render(<App />);
-  const linkElement = element.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
 
